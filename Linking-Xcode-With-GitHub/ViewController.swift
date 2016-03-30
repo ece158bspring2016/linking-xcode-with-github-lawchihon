b0e3818a479b9f9146dfab1fc428e597926e8ae1
John <lawchihon@ymail.com>
2016-03-30 16:26:09 -0700

    Initial Commit

---
+++ Linking-Xcode-With-GitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Linking-Xcode-With-GitHub
+//
+//  Created by John Law on 30/3/2016.
+//  Copyright © 2016 Chi Hon Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
